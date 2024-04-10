675f14f754cbd41754a1fd61ad95989185ec4454
Pat <makuchpatryk@gmail.com>
2024-04-10 16:13:28 +0200

    add context

--- src/pages/api/ranking.ts
+++ src/pages/api/ranking.ts
@@ -1,24 +1,24 @@
 import { NextApiHandler } from 'next'
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
 const handler: NextApiHandler = async (req, res) => {
-    const prisma = new PrismaClient()
+    // const prisma = new PrismaClient()
 
-    try {
-        const { name, level, finishTime } = req.body;
+    // try {
+    //     const { name, level, finishTime } = req.body;
 
-        const result = await prisma.ranking.create({
-            data: {
-                name: name,
-                level: level,
-                finishTime: finishTime,
-            }
-        });
-        res.json(result);
-      } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: error })
-      }
+    //     const result = await prisma.ranking.create({
+    //         data: {
+    //             name: name,
+    //             level: level,
+    //             finishTime: finishTime,
+    //         }
+    //     });
+    //     res.json(result);
+    //   } catch (error) {
+    //     console.error(error)
+    //     res.status(500).json({ message: error })
+    //   }
 }
 
 export default handler
--- src/pages/api/seed-data.ts
+++ src/pages/api/seed-data.ts
@@ -1,27 +1,27 @@
 import { NextApiHandler } from 'next'
-import { PrismaClient } from '@prisma/client'
-import { rankings } from '../../../prisma/seeds/rankings.js'
+// import { PrismaClient } from '@prisma/client'
+// import { rankings } from '../../../prisma/seeds/rankings.js'
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 const handler: NextApiHandler = async (req, res) => {
-  try {
-    await prisma.ranking.deleteMany()
-    
-    await Promise.all(
-        rankings.map(async (ranking: any) => {
-          await prisma.ranking.create({
-            data: ranking,
-          })
-        })
-      )
-    res.status(200).json({ message: 'Test data seeded successfully' })
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Error seeding test data' })
-  } finally {
-    await prisma.$disconnect()
-  }
+  // try {
+  //   await prisma.ranking.deleteMany()
+
+  //   await Promise.all(
+  //       rankings.map(async (ranking: any) => {
+  //         await prisma.ranking.create({
+  //           data: ranking,
+  //         })
+  //       })
+  //     )
+  //   res.status(200).json({ message: 'Test data seeded successfully' })
+  // } catch (error) {
+  //   console.error(error)
+  //   res.status(500).json({ message: 'Error seeding test data' })
+  // } finally {
+  //   await prisma.$disconnect()
+  // }
 }
 
 export default handler
